9c471fc57040b5a64d1dd4f51eec20b81df70a44
James <j.alsop99@hotmail.com>
2023-11-08 00:21:44 +0000

    Added homepage component

--- todo_app/frontend/src/components/App.js
+++ todo_app/frontend/src/components/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
+import HomePage from "./Homepage";
 
-export default function App(props){
-    return <h1>Testing React Code</h1>
-};
+export default function App(props) {
+  return (
+    <>
+      <h1>Testing React Code</h1>
+      <p>Hello {props.name}</p>
+
+      <HomePage />
+    </>
+  );
+}
 
 const appDiv = document.getElementById("app");
-render(<App/>, appDiv);
+render(<App name="jimbo" />, appDiv);
