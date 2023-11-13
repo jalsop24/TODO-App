731584d64bc6be077cefee878df5f8ba32189972
James <j.alsop99@hotmail.com>
2023-11-13 19:47:40 +0000

    Updated homepage

--- todo_app/frontend/src/components/Homepage.js
+++ todo_app/frontend/src/components/Homepage.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
 
 export default function HomePage(props) {
-  return <p>HomePage</p>;
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Home Page</h1>
+        <Link to="/todo-list"> TODO List </Link>
+      </main>
+    </>
+  );
 }
